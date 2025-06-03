6ec52593b69c015069c4c86dbce3a9ed2c6c8ab8
micropuma <micropuma@163.com>
2025-06-03 14:03:38 +0800

    [chore] add readme and env.sh

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # AIE-E2E
+This project supports E2E flow from pytorch to Versal AIE binary. 
+
+## References
+1. [Aries](https://github.com/arc-research-lab/Aries)
+2. [Stream-HLS](https://github.com/UCLA-VAST/Stream-HLS)
---
+++ env.sh
@@ -0,0 +1,5 @@
+# install torch-mlir
+pip install --pre torch-mlir torchvision \
+  --extra-index-url https://download.pytorch.org/whl/nightly/cpu \
+  -f https://github.com/llvm/torch-mlir-release/releases/expanded_assets/dev-wheels
+
